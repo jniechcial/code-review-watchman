98eb84e7e51ec386f6c78f793a8df2914e1feec3
Kuba Niechciał <jakub.niechcial@netguru.co>
2017-07-22 10:20:17 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# Code Review Watchman 👀
+Code Review Watchman is script tool that gives you statistical information about code review process in your organization on Github.
+
+# Using 🚀
+* Fetch the repository
+* Run `$ cp .env.example .env`
+* Fill in environment variables in `.env` file
+* Run `npm start`
+
+# Development & Contributing 🔗
+If you want to add new feature just create an issue and, if possible, create Pull Request that brings in that feature.
+
+To run tests run `$ npm test`.
+
+To debug using Node Inspect run `$ npm run debug`.
--- index.ts
+++ index.ts
@@ -1,16 +1,18 @@
+require('dotenv').config();
+
 import Watchman from './src/watchman';
 import GithubAdapter from './src/github-adapter';
 import MemoryCacheAdapter from './src/memory-cache-adapter';
 import RedisCacheAdapter from './src/redis-cache-adapter';
 
-const token : string = process.env.TOKEN || '';
-const username : string = process.env.USERNAME || '';
+const token : string = process.env.GITHUB_TOKEN || '';
+const username : string = process.env.GITHUB_USERNAME || '';
 
 const REDIS_DEFAULT_CACHE_KEY = '_watchman';
 
 const memoryCacheAdapter = new MemoryCacheAdapter();
-const redisCacheAdapter = new RedisCacheAdapter(process.env.REDIS_CACHE_KEY || REDIS_DEFAULT_CACHE_KEY);
-const githubAdapter = new GithubAdapter(token, username, process.env.ORGANIZATION || 'netguru', memoryCacheAdapter);
+// const redisCacheAdapter = new RedisCacheAdapter(process.env.REDIS_CACHE_KEY || REDIS_DEFAULT_CACHE_KEY);
+const githubAdapter = new GithubAdapter(token, username, process.env.GITHUB_ORGANIZATION || 'netguru', memoryCacheAdapter);
 
 const watchman = new Watchman(new Date(2017, 6, 15), new Date(2017, 6, 17), githubAdapter);
 
