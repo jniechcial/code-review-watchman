59d0995df8f59380183a41f4fde77fb9cb5174f3
Kuba Niechciał <jakub.niechcial@netguru.co>
2017-08-02 15:24:46 +0200

    add repository object that will aggregate created users

---
+++ src/repositories/users.test.ts
@@ -0,0 +1,19 @@
+import UsersRepository from './users';
+import User from '../models/user';
+import * as Errors from '../errors';
+
+const user = new User('test1');
+
+describe('UsersRepository', () => {
+    it('#getUser returns user by particular username', async () => {
+        const usersRepository = new UsersRepository([user]);
+        expect(usersRepository.getUser('test1')).toEqual(user);
+    });
+
+    it('#getUser throws NoUserError when no user is found for particular username', async () => {
+        const usersRepository = new UsersRepository([]);
+        expect(() => {
+            usersRepository.getUser('test1')
+        }).toThrowError(Errors.NoUserFound);
+    });
+});
